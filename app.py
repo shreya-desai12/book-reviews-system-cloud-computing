a8413dcd0fc7699b16565bc1e7686e62eaf7a84b
shreya-desai12 <37394940+shreya-desai12@users.noreply.github.com>
2021-04-28 23:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def book(isbn):
     if book is None:
         return render_template('error.html', message='This book is not available', navbar=True)
     
-    br = db.execute('SELECT * FROM reviews WHERE isbn=:isbn',{'isbn': isbn}).fetchall()
+    br = db.execute('SELECT * FROM reviews WHERE isbn=:isbn',{'isbn': isbn}).fetchone()
     isbn=br.isbn
     review=br.review
     username=br.user_name
